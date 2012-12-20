d775b626b5dd0e5ec1f38e1c3a269e38bc33e727
Paul Stemmet <paul.stemmet@gmail.com>
2012-12-20 16:07:11 +0200

    updated

---
+++ shinka-publisher-lib-php/config.php
@@ -0,0 +1,14 @@
+<?php 
+define('TIMEOUT',10);
+define('IS_RESIZE_IMAGES',TRUE);
+define('REFERER','randomjokes');
+define('TESTUSER','paulstemmet');
+
+define('API_SERVER','http://ox-d.shinka.sh/ma/1.0/arj');
+				
+define('AdUnitID_320',"330030");
+define('AdUnitID_216',"330030");
+define('AdUnitID_168',"330030");
+define('AdUnitID_120',"330030");		
+
+?>
